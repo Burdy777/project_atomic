64b41d06c36076b259465d9f6241942d039307c5
Burdy <bgoureige@keley-live.com>
2019-01-10 22:26:56 +0100

    app login

--- main/src/app/app.component.ts
+++ main/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { User } from 'src/model/user';
+import { LocalStorageService } from 'src/services/local-storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'main';
+  error: boolean;
+  userValid: User = { name: 'burdy', password: 'goureige'};
+  user: User = { name: '', password: ''};
+ constructor( private storage:LocalStorageService ) {}
+
+
+  public isSubmited (event: Event) {
+    const name: string = event.target[0].value;
+    const password: string = event.target[1].value;
+    const user = { name: name, password: password};
+    if (name === this.userValid.name && password === this.userValid.password) {
+      for (const key in user ) {
+        if (user.hasOwnProperty(key)) {
+          this.storage.setItem(key, user[key]);
+        }
+      }
+    } else {
+      this.error = true;
+    }
+    }
 }
---
+++ main/src/services/local-storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+constructor() { }
+/**
+ * set Item In localStorage
+ */
+public setItem(key: string, value: string) {
+  localStorage.setItem(name, value);
+}
+
+/**
+ * get Item In localStorage
+ */
+public getItem(key: string): string {
+  return localStorage.getItem(key);
+}
+
+/**
+ * get length propreties in localStorage
+ */
+public getCount():number {
+  return localStorage.length;
+}
+}
